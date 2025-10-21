89b2e0a4de4edd2853b137514e65fbd58a3a24b2
UjjwalSinghY <ujjwalsingh24090@gmail.com>
2025-10-21 22:06:11 +0530

    adding reverseArray logic inside static method

--- array/ReverseArray.java
+++ array/ReverseArray.java
@@ -1,8 +1,9 @@
-package array;
+package basic.array.array;
 
 public class ReverseArray {
-    public static void main(String[] args) {
-        int[] a={1,2,3,4,5};
+
+    public static void reverseArray(int[] a){
+
         for(int i=0;i<a.length/2;i++){
             int temp=a[i];
             a[i]=a[a.length-i-1];
@@ -11,5 +12,9 @@ public class ReverseArray {
         for(int i=0;i<a.length;i++){
             System.out.print(a[i]+" ");
         }
+    }
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5};
+       reverseArray(a);
     }
 }
